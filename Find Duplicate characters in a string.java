26c4f4edb6238334d2080c02ee152d8f984efe57
christopherjoyjayakumari <112762701+christopherjoyjayakumari@users.noreply.github.com>
2022-11-18 18:49:10 +0530

    Create Find Duplicate characters in a string.java

---
+++ Find Duplicate characters in a string.java
@@ -0,0 +1,40 @@
+package codeoftheday;
+
+public class Code5 
+{
+	static void duplicateCharCount()
+	{
+	String str="Dhaanish Engineering";
+		char[]strArray = str.toCharArray();
+		char duplicateAlphabet[]=new char[str.length()];
+		int duplicateCount[]=new int[str.length()];
+		int row=0;
+		for (char c:strArray)
+		{
+			char temp=c;int tempcount=0;
+			for(char cc:strArray) {
+				if(temp==cc) {
+					tempcount++;
+				}
+			}
+			duplicateAlphabet[row]=c;    duplicateCount[row]=tempcount;
+			row++;
+		}
+		char temp[]=new char[str.length()];
+		boolean print=true;
+		for(int i=0;i<str.length();i++) {
+			for(char t:temp) {
+				if(t==duplicateAlphabet[i]) {    print=false;	}
+			}
+			if(print) {
+				System.out.print("Character..."+duplicateAlphabet[i]+"...");
+				System.out.println("No of Tmes....:"+duplicateCount[i]);
+			}
+			temp[i]=duplicateAlphabet[i];
+			print=true;
+			}
+	}
+	public static void main(String[] args)
+	{ duplicateCharCount();
+	}
+}
